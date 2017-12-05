925537d0f2788b67f7d0f7626eeb391548cc780c
leol <ducleoliao@gmail.com>
2017-12-05 13:59:27 -0800

    Settup the 1st react component

---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Graph from './graph.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Graph />, root);
+});
---
+++ frontend/graph.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Graph extends React.Component {
+  
+  render(){
+    return (
+      <div className="graph-container">
+        <h1>This is react</h1>
+      </div>
+    ) // end return
+  } // render
+} // end Graph
+
+export default Graph;
