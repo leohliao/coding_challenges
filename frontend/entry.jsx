2cbbbe506fc6329a029b374f35d4525bab8b2161
leol <ducleoliao@gmail.com>
2018-01-24 11:25:56 -0800

    Changed folder structure

--- frontend/entry.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DataRender from './components/data_render';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<DataRender />, root);
-});
